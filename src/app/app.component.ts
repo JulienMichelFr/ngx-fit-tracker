e725da3e91dfeca53194f4a05b27a17bac317c6c
Julien Michel <julienmichel.pro@gmail.com>
2019-05-09 22:48:14 +0200

    update style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,8 @@ export class AppComponent implements AfterViewInit {
         scales: {
           yAxes: [{
             ticks: {
-              beginAtZero: true
+              beginAtZero: true,
+              min: 110
             }
           }]
         }
