d5a8ec18fc27cfd072373f0a5082ede134636967
Julien Michel <julienmichel.pro@gmail.com>
2020-09-13 12:09:05 +0200

    update display

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -47,7 +47,7 @@ export class ChartComponent implements AfterViewInit, OnChanges {
         ),
         datasets: [
           {
-            label: 'Poids',
+            label: 'Weight',
             data: (this.data || []).map(({ value }) => value),
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
@@ -59,11 +59,6 @@ export class ChartComponent implements AfterViewInit, OnChanges {
             ],
             borderColor: [
               'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 3
           }
